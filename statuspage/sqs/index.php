ca1f176430601ca91a9092c61246a911c62d63bc
Dennis Foster <54544194+dennisfoster@users.noreply.github.com>
2019-12-05 10:54:42 -0700

    Update index.php

--- statuspage/sqs/index.php
+++ statuspage/sqs/index.php
@@ -52,7 +52,7 @@ $mysqli->close();
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
-        <meta http-equiv="refresh" content="1">
+        <meta http-equiv="refresh" content="5">
         <title>AccuMed SQS Monitor</title>
     </head>
     <body>
