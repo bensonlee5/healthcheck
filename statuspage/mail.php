b6343e01882949230572fa80b5ba8dcb9ff15208
Dennis Foster <54544194+dennisfoster@users.noreply.github.com>
2019-12-06 16:08:48 -0700

    Update mail.php

--- statuspage/mail.php
+++ statuspage/mail.php
@@ -1,4 +1,4 @@
 <?php
     echo shell_exec('echo -e "Subject: Healthcheck Alert!\n\nHealthcheck Alert:\n' .
-        $_GET['endpoint'] . ' is down." | ssmtp sysadmin@accumedintel.com');
+        $_GET['endpoint'] . ' is down." | ssmtp systemadministrators@accumedintel.com');
 ?>
