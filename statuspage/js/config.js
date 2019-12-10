98c03bb464e225172d34eb70bc8342dec7aae4a6
Dennis Foster <54544194+dennisfoster@users.noreply.github.com>
2019-12-10 10:44:15 -0700

    Update config.js

--- statuspage/js/config.js
+++ statuspage/js/config.js
@@ -2,7 +2,7 @@ checkup.config = {
 	// How much history to show on the status page. Long durations and
 	// frequent checks make for slow loading, so be conservative.
 	// This value is in NANOSECONDS to mirror Go's time package.
-	"timeframe": 1 * time.Day,
+	"timeframe": 1 * time.Hour,
 
 	// How often, in seconds, to pull new checks and update the page.
 	"refresh_interval": 10,
