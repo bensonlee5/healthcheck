7d0c51bd6054737b57e72e1fbad0269e0ba2508a
Dennis Foster <54544194+dennisfoster@users.noreply.github.com>
2019-12-10 12:13:33 -0700

    Update mail.php

--- statuspage/mail.php
+++ statuspage/mail.php
@@ -1,14 +1,19 @@
 <?php
-$new_status = [];
-$files = scandir('/var/www/healthcheck.accumedintel.net/statuspage/check_files', SCANDIR_SORT_DESCENDING);
-$newest_file = $files[0];
-$data = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/check_files/' . $newest_file));
-$status = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json'));
-foreach ($data as $endpoint) {
-    if (!in_array($endpoint->title, $status) && $endpoint->down) {
-        //send mail
-        $new_status[] = $endpoint->title;
+while(1) {
+    sleep(60);
+    $new_status = [];
+    $files = scandir('/var/www/healthcheck.accumedintel.net/statuspage/check_files', SCANDIR_SORT_DESCENDING);
+    $newest_file = ($files[0] == 'index.json') ? $files[1] : $files[0];
+    $data = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/check_files/' . $newest_file));
+    $status = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json'));
+    foreach ($data as $endpoint) {
+        if (!in_array($endpoint->title, $status) && isset($endpoint->down)) {
+            //send mail
+            shell_exec('echo -e "Subject: Healthcheck Alert!\n\nHealthcheck Alert:\n' .
+            $endpoint->title . ' is down." | ssmtp d.foster@accumedintel.com');
+            $new_status[] = $endpoint->title;
+        }
     }
+    file_put_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json', json_encode($new_status));
 }
-file_put_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json', json_encode($new_status));
 ?>
