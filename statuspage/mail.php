d62c61e3f376ee09c724444155e94abc6a5a48e0
Dennis Foster <d.foster@accumedintel.com>
2019-12-10 14:49:11 -0700

    Remove old checkup files

--- statuspage/mail.php
+++ statuspage/mail.php
@@ -3,6 +3,11 @@ while(1) {
     sleep(60);
     $new_status = [];
     $files = scandir('/var/www/healthcheck.accumedintel.net/statuspage/check_files', SCANDIR_SORT_DESCENDING);
+    foreach ($files as $i => $file) {
+        if ($i > 70) {
+            unlink('/var/www/healthcheck.accumedintel.net/statuspage/check_files/' . $file);
+        }
+    }
     $newest_file = ($files[0] == 'index.json') ? $files[1] : $files[0];
     $data = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/check_files/' . $newest_file));
     $status = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json'));
