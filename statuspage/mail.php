239a712f1578e2bfe49e70c7338d495a5dab81a4
Dennis Foster <d.foster@accumedintel.com>
2019-12-10 11:53:58 -0700

    Add mailer worker

--- statuspage/mail.php
+++ statuspage/mail.php
@@ -1,4 +1,14 @@
 <?php
-    echo shell_exec('echo -e "Subject: Healthcheck Alert!\n\nHealthcheck Alert:\n' .
-        $_GET['endpoint'] . ' is down." | ssmtp systemadministrators@accumedintel.com');
+$new_status = [];
+$files = scandir('/var/www/healthcheck.accumedintel.net/statuspage', SCANDIR_SORT_DESCENDING);
+$newest_file = $files[0];
+$data = json_decode(file_get_contents($newest_file));
+$status = json_decode(file_get_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json'));
+foreach ($data as $endpoint) {
+    if (!in_array($endpoint->title, $status) && $endpoint->down) {
+        //send mail
+        $new_status[] = $endpoint->title;
+    }
+}
+file_put_contents('/var/www/healthcheck.accumedintel.net/statuspage/status.json', json_encode($new_status));
 ?>
